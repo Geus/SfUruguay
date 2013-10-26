ac9160141b0f5e4ce15321091adfc08e96e775a2
Guillermo Bergengruen <gbergengruen@gmail.com>
2013-10-26 13:24:13 -0200

    added the namespace

--- src/SfUruguay/TestBundle/Entity/User.php
+++ src/SfUruguay/TestBundle/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
  * User
  *
  * @ORM\Table(name="users")
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="SfUruguay\TestBundle\Entity\UserRepository")
  */
 class User
 {
